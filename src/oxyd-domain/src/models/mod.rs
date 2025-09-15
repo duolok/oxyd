2252b3fcce6b22d1fed5fb4ad1a559157d49782c
duolok <dusan.lecic5@gmail.com>
2025-09-15 22:06:48 +0200

    update: mod
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

--- src/oxyd-domain/src/models/mod.rs
+++ src/oxyd-domain/src/models/mod.rs
@@ -2,8 +2,12 @@ pub mod system;
 pub mod process;
 pub mod metrics;
 pub mod config;
+pub mod errors;
+pub mod plugin;
 
 pub use system::*;
 pub use process::*;
 pub use metrics::*;
 pub use config::*;
+pub use errors::*;
+pub use plugin::*;
