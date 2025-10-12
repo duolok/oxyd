f6742841a35597c427c6846c25ef0c6a35eb6f6e
duolok <dusan.lecic5@gmail.com>
2025-10-12 18:12:23 +0200

    remove: useless comment
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

--- src/oxyd-process-manager/src/linux/mod.rs
+++ src/oxyd-process-manager/src/linux/mod.rs
@@ -5,7 +5,6 @@ mod helpers;
 
 pub use manager::LinuxProcessManager;
 
-// Re-export internal types if needed
 pub(crate) use cpu::{CpuMeasurement, calculate_cpu_usage_cached, calculate_cpu_usage_instant};
 pub(crate) use parsers::{StatFields, StatusFields, parse_stat, parse_status};
 pub(crate) use helpers::{count_connections, get_boot_time, calculate_memory_percent};
